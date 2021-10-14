459997a10eab6d4e84c3ca425c29a3f073a48e1d
ryser <realramsangle@gmail.com>
2021-10-14 12:39:05 +0530

    Added Proper Comments

--- Cpp/Test.cpp
+++ Cpp/Test.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int main() {
+    //Declare variables
     string a, b;
+
+    //Take Inputs
     cout << "First Name:- ";
     cin >> a;
     cout << "Second Name:- ";
     cin >> b;
+
+    //Show Outputs
     cout << "Hello " << a << " " << b; 
     return 0;
 }
