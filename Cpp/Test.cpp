c88315e05520a98987d44cd3bcaf7e8515e11682
ryser <realramsangle@gmail.com>
2021-10-14 12:26:13 +0530

    cpp test

---
+++ Cpp/Test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a, b;
+    cout << "First Name:- ";
+    cin >> a;
+    cout << "Second Name:- ";
+    cin >> b;
+    cout << "Hello " << a << " " << b; 
+    return 0;
+}
